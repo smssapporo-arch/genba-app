d7d88c0639a96a511cd3e534ca96534fdabb6b2b
smssapporo-arch <sms.sapporo@gmail.com>
2026-09-24 16:06:41 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+/* アプリ本体をキャッシュして、オフラインでも即起動できるようにする。
+   index.html などを直したら CACHE の番号を上げること。 */
+const CACHE = 'genba-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // Apps Script への通信はキャッシュしない
+  if (url.origin !== location.origin) return;
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
